85b976daae1a170647dc54b28a51d98b1e2b52b5
Lei Mao <dukeleimao@gmail.com>
2017-08-22 13:29:28 +0800

    d

---
+++ image_resizer.py
@@ -0,0 +1,27 @@
+# Image Resizer
+# Author: Lei Mao
+# Institution: University of Chicago
+# Date: 8/22/2017
+# Python: 3.6
+
+
+import cv2
+
+def resizer_ratio(img_path, img_name, fx = 1, fy = 1):
+    img_name_prefix = img_name.split('.')[0]
+    img = cv2.imread(img_path + img_name)
+    res = cv2.resize(src = img, dst = None, fx = fx, fy = fy, interpolation = cv2.INTER_AREA)
+    cv2.imwrite(filename = img_path + img_name_prefix + '_resized.png', img = res)
+    return
+
+def resizer_defined(img_path, height = None, width = None):
+    img_name_prefix = img_name.split('.')[0]
+    img = cv2.imread(img_path + img_name)
+    res = cv2.resize(src = img, dst = (width, height), fx = 0, fy = 0, interpolation = cv2.INTER_AREA)
+    cv2.imwrite(filename = img_path + img_name_prefix + '_resized.png', img = res)
+    return 
+
+
+
+
+
