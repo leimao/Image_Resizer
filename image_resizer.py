434be7b7ada9345b6417e3249ebe1b44e4527e8d
Lei Mao <dukeleimao@gmail.com>
2017-08-22 18:09:32 +0800

    d

--- image_resizer.py
+++ image_resizer.py
@@ -10,14 +10,14 @@ import cv2
 def resizer_ratio(img_path, img_name, fx = 1, fy = 1):
     img_name_prefix = img_name.split('.')[0]
     img = cv2.imread(img_path + img_name)
-    res = cv2.resize(src = img, dst = None, fx = fx, fy = fy, interpolation = cv2.INTER_AREA)
+    res = cv2.resize(src = img, dsize = None, fx = 0.2, fy = 0.2, interpolation = cv2.INTER_AREA)
     cv2.imwrite(filename = img_path + img_name_prefix + '_resized.png', img = res)
     return
 
 def resizer_defined(img_path, height = None, width = None):
     img_name_prefix = img_name.split('.')[0]
     img = cv2.imread(img_path + img_name)
-    res = cv2.resize(src = img, dst = (width, height), fx = 0, fy = 0, interpolation = cv2.INTER_AREA)
+    res = cv2.resize(src = img, dsize = (width, height), fx = 0, fy = 0, interpolation = cv2.INTER_AREA)
     cv2.imwrite(filename = img_path + img_name_prefix + '_resized.png', img = res)
     return 
 
